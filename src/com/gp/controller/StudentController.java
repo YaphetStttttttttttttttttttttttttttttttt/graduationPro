3d0a52f5abf24078a4a7601bf7f0ab1f22fdf1d5
YaphetStttttttttttttttttttttttttttttttt <493051632@qq.com>
2020-03-24 12:47:02 +0800

    name

--- src/com/gp/controller/StudentController.java
+++ src/com/gp/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,16 +20,15 @@ public class StudentController {
     public ModelAndView listCategory(){
         ModelAndView mav = new ModelAndView();
         List<Student> ss= studentService.list();
-         
         // 放入转发参数
         mav.addObject("ss", ss);
         // 放入jsp路径
         mav.setViewName("listStudent");
         return mav;
     }
- /*   @RequestMapping(value="{name}")
+    @RequestMapping(value="{name}")
 	public String commonHandler(@PathVariable String name) {
 		return name;
 	}
-*/
+
 }
